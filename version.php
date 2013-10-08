f3ebf6e55da8e95e4fecb41a6a04d248373166fe
Jim Pease <jmpease@codeserv.us>
2013-10-07 23:20:09 -0400

    Added recommended version info.

--- version.php
+++ version.php
@@ -14,6 +14,8 @@ defined('MOODLE_INTERNAL') || die();
 // You are on the MOODLE_24_STABLE branch.  Do NOT update the version
 // branching date rather update the release increment.
 $plugin->version        = 2013082700;
+$plugin->release        = '2.4 (Build: 2013082700)';
+$plugin->maturity       = MATURITY_STABLE;
 $plugin->requires       = 2012062500;
 $plugin->component      = 'repository_ensemble';
 $plugin->dependencies   = array('filter_ensemble' => 2013040900);

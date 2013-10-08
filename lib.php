f20524c708edeee04bef0c473ed1d22863e5eee6
Jim Pease <jmpease@codeserv.us>
2013-10-08 00:26:12 -0400

    Updated licensing to follow coding style as recommended.

--- lib.php
+++ lib.php
@@ -1,27 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// Repository to manage publish interface with Ensemble server
-// Written by Liam Moran, March of 2012 <moran@illinois.edu>
-// Updated by Liam Moran, Dec. of 2012
-// This is for version 2.3--non-operational with older versions of Moodle
-//
-//    Copyright (C) 2012 Liam Moran
-//    Copyright (C) 2013 Symphony Video, Inc.
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//
+
+/**
+ * Ensemble Video repository plugin.
+ *
+ * @package    repository_ensemble
+ * @copyright  2012 Liam Moran, Nathan Baxley, University of Illinois
+ *             2013 Symphony Video, Inc.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 class repository_ensemble extends repository {
 

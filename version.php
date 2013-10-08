696189eef41426ec1428ac30df68378c558c6e5b
Jim Pease <jmpease@codeserv.us>
2013-10-08 00:26:12 -0400

    Updated licensing to follow coding style as recommended.

--- version.php
+++ version.php
@@ -1,13 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// This repository provides an interface to an Ensemble server
-// and presents the media assigned to a course via an Ensemble directory
-// as well as a search interface to add other media available in a system-wide
-// public catalog.
-//
-// It was written by Liam Moran <moran@illinois.edu> in March, 2012
-//
-// GPL language goes here.
+
+/**
+ * Ensemble Video repository plugin.
+ *
+ * @package    repository_ensemble
+ * @copyright  2012 Liam Moran, Nathan Baxley, University of Illinois
+ *             2013 Symphony Video, Inc.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
